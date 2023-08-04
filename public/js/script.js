f8dc9ce217c1e9d1079162c54800d114929ce44d
unknown <asharisulthan16@gmail.com>
2023-08-04 17:08:51 +0700

    Website Solit IT 03

---
+++ public/js/script.js
@@ -0,0 +1,69 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector ('.navbar-nav');
+
+// Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Toggle class active untuk shooping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+};
+
+// Klik di luar elemen
+const hamburger = document.querySelector('#hamburger-menu');
+const searchbutton = document.querySelector('#search-button');
+const shoppingcart = document.querySelector('#shopping-cart-button')
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+
+    if (!searchbutton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active')
+    }
+
+    if (!shoppingcart.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active')
+    }
+});
+
+
+// Modal Box 
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+    };
+});
+
+
+// Klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+// Klikn di luar modal 
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        modal.style.display = 'none';
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="english">
+  <head>
+    <title>Solit Computer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" />
+    <meta property="twitter:card" content="summary_large_image" />
+
+    <style data-tag="reset-style-sheet">
+      html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
+    </style>
+    <style data-tag="default-style-sheet">
+      html {
+        font-family: Inter;
+        font-size: 16px;
+      }
+
+      body {
+        font-weight: 400;
+        font-style:normal;
+        text-decoration: none;
+        text-transform: none;
+        letter-spacing: normal;
+        line-height: 1.15;
+        color: var(--dl-color-gray-black);
+        background-color: var(--dl-color-gray-white);
+
+      }
+    </style>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+      data-tag="font"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="icon" href="img/solit.jpg" type="image/x-icon"/>
+  </head>
+  <body>
+    <div>
+      <link href="css/home.css" rel="stylesheet" />
+
+      <div class="desktop1-container">
+        <div class="desktop1-desktop1">
+          <a
+            href="https://www.tokopedia.com/solit03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link"
+          >
+            <img
+              src="img/rectangle1button832-v7f-200h.png"
+              alt="Rectangle1button832"
+              class="desktop1-rectangle1button"
+            />
+          </a>
+          <a
+            href="https://shopee.co.id/solit_03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link1"
+          >
+            <img
+              src="img/rectangle2button833-bkwe-200h.png"
+              alt="Rectangle2button833"
+              class="desktop1-rectangle2button"
+            />
+          </a>
+          <a
+            href="https://www.tiktok.com/@solusi_it03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link2"
+          >
+            <img
+              src="img/rectangle3button837-a5fh-200h.png"
+              alt="Rectangle3button837"
+              class="desktop1-rectangle3button"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/solit.comp/?hl=en"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link3"
+          >
+            <img
+              src="img/rectangle4button838-aexk-200h.png"
+              alt="Rectangle4button838"
+              class="desktop1-rectangle4button"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100089604309514&amp;mibextid=ZbWKwL"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link4"
+          >
+            <img
+              src="img/rectangle6button840-kz5o-200h.png"
+              alt="Rectangle6button840"
+              class="desktop1-rectangle6button"
+            />
+          </a>
+          <a
+            href="https://www.google.com/maps?q=SOLIT+IT+03,+RT+06+RW+05+no+57,+rangkapan+jaya+lama,+Kec.+Pancoran+Mas,+Kota+Depok,+Jawa+Barat&amp;ftid=0x2e69e9a37414be5f:0xb6eb31f91cc6287e&amp;hl=id-ID&amp;gl=id&amp;entry=gps&amp;lucs=47062714&amp;g_ep=CAISBjYuNTYuMhgAINeCAyoINDcwNjI3MTRCAklE&amp;g_st=iwb"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link5"
+          >
+            <img
+              src="img/rectangle7button1313-x2sc-200h.png"
+              alt="Rectangle7button1313"
+              class="desktop1-rectangle7button"
+            />
+          </a>
+          <a
+            href="https://t.me/solitcomputer_bot"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link6"
+          >
+          <img
+            src="img/rectangle8button1378-2zn6-200h.png"
+            alt="Rectangle8button1378"
+            class="desktop1-rectangle8button"
+          />
+        </a>
+        <a
+            href="https://api.whatsapp.com/send/?phone=6285930464669&amp;text&amp;type=phone_number&amp;app_absent=0"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link7"
+          >
+            <img
+              src="img/rectangle6button840-kz5o-200h.png"
+              alt="Rectangle6button840"
+              class="desktop1-rectangle6button1"
+            />
+          </a>
+
+
+        {{-- Logo Sosial Media --}}
+        <a
+          href="https://www.facebook.com/profile.php?id=100089604309514&amp;mibextid=ZbWKwL"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="desktop1-link8"
+          >
+          <img
+            src="img/facebook.png"
+            alt="facebook1842"
+            class="desktop1-facebook1"
+          />
+          </a>
+
+          <a
+            href="https://www.instagram.com/solit.comp/?hl=en"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link9"
+          >
+          <img
+            src="img/instagram.png"
+            alt="instagram1124"
+            class="desktop1-instagram1"
+          />
+          </a>
+
+          <a
+            href="https://www.tiktok.com/@solusi_it03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link10"
+          >
+          <img
+            src="img/tiktok.png"
+            alt="tiktok1125"
+            class="desktop1-tiktok1"
+          />
+          </a>
+
+          <a
+            href="https://shopee.co.id/solit_03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link11"
+          >
+          <img
+            src="img/shopee.png"
+            alt="shopee1136"
+            class="desktop1-shopee1"
+          />
+          </a>
+
+          <a
+            href="https://www.tokopedia.com/solit03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link12"
+          >
+          <img
+            src="img/tokopedia.png"
+            alt="tokopedia1137"
+            class="desktop1-tokopedia1"
+          />
+          </a>
+
+          <a
+            href="https://www.google.com/maps?q=SOLIT+IT+03,+RT+06+RW+05+no+57,+rangkapan+jaya+lama,+Kec.+Pancoran+Mas,+Kota+Depok,+Jawa+Barat&amp;ftid=0x2e69e9a37414be5f:0xb6eb31f91cc6287e&amp;hl=id-ID&amp;gl=id&amp;entry=gps&amp;lucs=47062714&amp;g_ep=CAISBjYuNTYuMhgAINeCAyoINDcwNjI3MTRCAklE&amp;g_st=iwb"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link13"
+          >
+          <img
+            src="img/maps.png"
+            alt="maps11314"
+            class="desktop1-maps1"
+          />
+          </a>
+
+          <a
+            href="https://t.me/solitcomputer_bot"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link14"
+          >
+          <img
+            src="img/telegram.png"
+            alt="telegram11379"
+            class="desktop1-telegram1"
+          />
+          </a>
+
+          <a
+            href="https://api.whatsapp.com/send/?phone=6285930464669&amp;text&amp;type=phone_number&amp;app_absent=0"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-link7"
+          >
+          <img
+            src="img/whatsapp1123-nind-200h-200h.png"
+            alt="whatsapp1123"
+            class="desktop1-whatsapp1"
+          />
+          </a>
+
+
+          {{-- Text Social Media --}}
+          <a
+            href="https://www.tokopedia.com/solit03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text04"
+          >
+          <span>Tokopedia</span>
+          </a>
+
+          <a
+            href="https://shopee.co.id/solit_03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text06"
+          >
+          <span>Shopee</span>
+          </a>
+
+          <a
+            href="https://www.tiktok.com/@solusi_it03"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text08"
+          >
+          <span>Tiktok</span>
+          </a>
+
+          <a
+            href="https://www.facebook.com/profile.php?id=100089604309514&amp;mibextid=ZbWKwL"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text10"
+          >
+          <span>Facebook</span>
+          </a>
+
+          <a
+            href="https://www.google.com/maps?q=SOLIT+IT+03,+RT+06+RW+05+no+57,+rangkapan+jaya+lama,+Kec.+Pancoran+Mas,+Kota+Depok,+Jawa+Barat&amp;ftid=0x2e69e9a37414be5f:0xb6eb31f91cc6287e&amp;hl=id-ID&amp;gl=id&amp;entry=gps&amp;lucs=47062714&amp;g_ep=CAISBjYuNTYuMhgAINeCAyoINDcwNjI3MTRCAklE&amp;g_st=iwb"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text12"
+          >
+          <span>Lokasi</span>
+          </a>
+
+          <a
+            href="https://t.me/solitcomputer_bot"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text14"
+          >
+          <span>Telegram</span>
+          </a>
+
+          <a
+            href="https://www.instagram.com/solit.comp"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text16"
+          >
+          <span>Instagram</span>
+          </a>
+
+          <a
+            href="https://api.whatsapp.com/send/?phone=6285930464669&amp;text&amp;type=phone_number&amp;app_absent=0"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="desktop1-text27"
+          >
+          <span>WhatsApp</span>
+          </a>
+
+          
+          <img
+            src="img/rectangle91415-24h-500h.png"
+            alt="Rectangle91415"
+            class="desktop1-rectangle9"
+          />
+          <img
+            src="img/logosolit1415-1bc.svg"
+            alt="LogoSolit1415"
+            class="desktop1-logo-solit1"
+          />
+          <span class="desktop1-text18"><span>Halo, selamat datang</span></span>
+          <span class="desktop1-text20">
+            Temukan informasi seputar laptop yang&nbsp; kamu butuhkan di website
+            ini.
+          </span>
+          <span class="desktop1-text21">Toko amanah yang pasti bergaransi</span>
+          <span class="desktop1-text22">
+            <span class="desktop1-text23">Cari</span>
+            <span class="desktop1-text24">laptop</span>
+            <span class="desktop1-text25">....Ada</span>
+            <span>Solit</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
