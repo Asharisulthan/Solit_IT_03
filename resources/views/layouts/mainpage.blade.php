a4e4a0a8e0455863c0af649d29c5d68158595585
asharisulthan <asharisulthan16@gmail.com>
2023-10-09 16:18:35 +0700

    new kode

--- resources/views/layouts/mainpage.blade.php
+++ resources/views/layouts/mainpage.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Solit Computer er</title>
+    <title>Solit Computer ER</title>
 
     {{-- Logo Solit --}}
     <link rel="icon" href="img/solit.jpg" type="image/x-icon"/>
